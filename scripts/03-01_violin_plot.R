3cc0e11a4c5aee2cc9a0e4b7ce0027df94908212
zachkaupp <zachkaupp@gmail.com>
2022-06-15 16:14:15 -0500

    make violin plots prettier

--- scripts/03-01_violin_plot.R
+++ scripts/03-01_violin_plot.R
@@ -5,29 +5,36 @@ cytokine_list <- list("IL-1β", "IFN-α2", "IFN-γ", "TNF-α",	"MCP-1",	"IL-6",
                        "IL-23",	"IL-33")
 
 plot_violin <- function(cytokine = "IL-1β", timepoint_index = 1) {
+  
+  # make the plot, most of this is for appearance
   x <- ggplot(lplex_normal_list_timepoints[[timepoint_index]],
               aes(x = STIM,
-                  y = lplex_normal_list_timepoints[[timepoint_index]][[cytokine]])) + 
-    geom_violin() +
+                  y = lplex_normal_list_timepoints[[timepoint_index]][[cytokine]],
+                  fill = STIM)) + 
+    geom_violin(trim = FALSE, color = "black") +
+    geom_boxplot(width = .7, outlier.shape = NA, color = "darkblue", fill = NA) +
     geom_dotplot(binaxis = 'y', stackdir = "center",
-                 dotsize = 1, alpha = .7, binwidth = .2) +
+                 dotsize = .7, alpha = .7,
+                 binwidth = (diff(range(lplex_normal_list_timepoints[[timepoint_index]][[cytokine]]))/20), # this makes sure the the dots stay around the same size for every cytokine, it is a problem otherwise
+                 color = "black",
+                 fill = "white") + 
     facet_wrap(~GROUP) +
-    labs(title = paste("Cytokine", cytokine, "at TIMEPOINT",
+    labs(title = paste("TIMEPOINT",
                        levels(factor(lplex_normal_list_timepoints[[timepoint_index]]$TIMEPOINT)),
-                       "between GROUPS, log-2-fold-change")) +
+                       "-- log-2-fold-change")) +
     xlab("STIM") +
-    ylab(cytokine)
-  #print(paste("Plotting", cytokine, "for TIMEPOINT",
+    ylab(cytokine) +
+    theme_light()
+  #print(paste("Plotting", cytokine, "for TIMEPOINT", # this will tell you when it plots, if you want that
               #levels(factor(lplex_normal_list_timepoints[[timepoint_index]]$TIMEPOINT))))
-  return(x)
+  return(x) # return the complete violin plot
 }
 
 print(paste("To access plot, run plot_violin(cytokine, timepoint_index). Choose from",
             length(lplex_normal_list_timepoints), "TIMEPOINTs."))
+print("Plots are also stored in output/violin_plot")
 
-plot_list <- list()
-#added <- 0
-for (i in cytokine_list) {
-  plot_list <- c(plot_list, list(plot_violin(i,1)))
-  #added <- added + 1
-}
+## OUTPUT ---
+
+
+
