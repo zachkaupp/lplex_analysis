ec874a1e69b313144c13f965e2d23f894084a2b6
zachkaupp <zachkaupp@gmail.com>
2022-06-29 10:12:24 -0500

    add significance test file

---
+++ scripts/06_significance_test.R
@@ -0,0 +1,8 @@
+## Record p-values for each analyte between groups
+
+# make settings to select the two groups to compare against each other
+# output format in csv: analyte, p-value
+# print the two groups that are being compared
+
+
+cat(cyan("Process complete\n"))
